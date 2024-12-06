37eed2682d092c0d56e60319f57f4397ee3c60e1
mnomanuel <u2636629@uel.ac.uk>
2024-12-06 09:49:10 +0000

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import FacebookEmojiCounter from './Facebookemoji';
+
+function App() {
+  return (
+    <div className="App">
+      <FacebookEmojiCounter/>
+    </div>
+  );
+}
+
+export default App;
